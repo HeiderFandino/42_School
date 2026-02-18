884dd7d37a522e819cd1bd8f94050d16c48d45ed
heiderfandino <heiderfandino@gmaiol.com>
2026-02-18 17:57:43 +0100

    prueva desde mi portatil con la pesada de tania.

--- ejercicios/ex/C07/ex00/prueba.c
+++ ejercicios/ex/C07/ex00/prueba.c
@@ -6,7 +6,7 @@
 /*   By: hfandino <hfandino@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 02:14:01 by hfandino          #+#    #+#             */
-/*   Updated: 2026/02/18 02:29:42 by hfandino         ###   ########.fr       */
+/*   Updated: 2026/02/18 17:53:46 by hfandnino        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,15 @@ int	ft_putstr(char	*str)
 	write(1, "\n", 1);
 	return (i);
 }
+/*esto es una prueba de que estuve aqui y se sincorino todo*/
 
 int	main(void)
 {
 	char	*str;
 	int		reslt;
-
 	str = "Hola mundo";
 	reslt = ft_putstr(str);
 	printf("%i \n", reslt);
 	return (0);
+
 }
